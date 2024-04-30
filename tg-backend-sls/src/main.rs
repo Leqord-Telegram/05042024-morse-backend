87c1582f6fb50892f841a72d7e06131168168301
Leqord <illigarium@gmail.com>
2024-05-01 00:15:23 +1000

    пилим

--- tg-backend-sls/src/main.rs
+++ tg-backend-sls/src/main.rs
@@ -1,6 +1,34 @@
-fn main() {
-    let age = 21;
-    let name = "Vasya";
+use std::env;
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
-    println!("Name = {} Age = {}", name, age);
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let host = env::var("HOST").unwrap_or_else(|_| "127.0.0.1".to_string());
+    let port = env::var("PORT").unwrap_or_else(|_| "8080".to_string());
+
+    println!("Listening on {}:{}", host, port);
+
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
