30a027b36378fed1d6903ca3533752432b01406b
Leqord <illigarium@gmail.com>
2024-05-01 23:39:44 +1000

    ID категории для продукта

--- tg-backend-sls/src/model/product.rs
+++ tg-backend-sls/src/model/product.rs
@@ -7,6 +7,7 @@ pub struct Product {
     pub id: i64,
     pub name: String,
     pub destription: String,
+    pub category_id: i64,
     pub price: i64,
     pub quantity: i64,
     pub active: bool,
