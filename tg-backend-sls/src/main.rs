294ba830f05a461af9f8a62929b6580ceeff7cd7
Leqord <illigarium@gmail.com>
2024-05-02 00:19:48 +1000

    пилим

--- tg-backend-sls/src/main.rs
+++ tg-backend-sls/src/main.rs
@@ -1,33 +1,14 @@
 mod model;
-use model::order;
 
 mod storage;
 use storage::memory::*;
 
 use std::env;
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, post, App, HttpResponse, HttpServer, Responder};
 
 
-#[get("/json1")]
-async fn json1() -> impl Responder {
-    let item = order::Item {
-        id: 123,
-        product_id: 512,
-        quantity: 124513,
-    };
-
-    let order = order::Order{
-        id: 12132,
-        user_id: 7345,
-        items: vec! [item,],
-        status: order::Status::Failed
-    };
-
-    HttpResponse::Ok().json(order)
-}
-
-#[get("/")]
-async fn hello() -> impl Responder {
+#[get("/categories")]
+async fn categories() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
@@ -36,24 +17,19 @@ async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
 }
 
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
-}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let host = env::var("HOST").unwrap_or_else(|_| "127.0.0.1".to_string());
     let port = env::var("PORT").unwrap_or_else(|_| "8080".to_string());
+
     let mut storage = MemoryStorage::new();
 
     println!("Listening on {}:{}", host, port);
 
     HttpServer::new(|| {
         App::new()
-            .service(hello)
             .service(echo)
-            .service(json1)
-            .route("/hey", web::get().to(manual_hello))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
