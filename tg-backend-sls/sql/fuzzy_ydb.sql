51a791176dd41671a0633a11a2b3c429abec27a4
Leqord <illigarium@gmail.com>
2024-05-03 00:15:31 +1000

    поиск

--- tg-backend-sls/sql/fuzzy_ydb.sql
+++ tg-backend-sls/sql/fuzzy_ydb.sql
@@ -5,18 +5,19 @@ $splitstr = ($str) -> {
     return Unicode::SplitToList(Unicode::Fold(Unicode::RemoveAll($str, $filtr), "Russian" AS Language), $sep)
 };
 
-$strRef = "шоссеандра   (шла)";
-$RefSplit = $splitstr($strRef);
-$RefTable = select source_word from (select $RefSplit  as source_word) flatten by source_word; 
-
-$strSearch = "шоссе шоа";
+$strSearch = "шоссе и шоа";
 $SearchSplit = $splitstr($strSearch);
 $SearchTable = select search_word from (select $SearchSplit  as search_word) flatten by search_word; 
 
-$compareTable = select * from $SearchTable as st cross join $RefTable as rt;
-$min_length = select min_of(Unicode::GetLength(search_word), Unicode::GetLength(source_word)) as min_length from $compareTable;
 
-select * from $compareTable;
+$orsplit = select id, $splitstr(text) as list_text from `search-levenstein`;
+
+$RefTable = select id, list_text as source_word from $orsplit flatten by list_text;
+
+$compareTable = select * from $SearchTable as st cross join $RefTable as rt;
+
+select id, Unicode::LevensteinDistance(search_word, source_word) as distance from $compareTable;
+
 
 -- мб и не надо или только для подсказок:
 -- select Unicode::Substring(source_word, 0, min_of(Unicode::GetLength(source_word), Unicode::GetLength(search_word))) as source_word_part, search_word from $compareTable;
