e45509eae2d4ab3f1b599e3dd0ff9f0d09eb6d6d
Leqord <illigarium@gmail.com>
2024-05-02 17:16:57 +1000

    Пофиксил инициализацию AppData

--- tg-backend-sls/src/main.rs
+++ tg-backend-sls/src/main.rs
@@ -66,7 +66,9 @@ async fn main() -> std::io::Result<()> {
     let host = env::var("HOST").unwrap_or_else(|_| "127.0.0.1".to_string());
     let port = env::var("PORT").unwrap_or_else(|_| "8080".to_string());
 
-    let app_state = web::Data::new(AppState::new());
+    let appstate = AppState::new().await;
+
+    let app_state = web::Data::new(appstate);
 
     println!("Listening on {}:{}", host, port);
 
