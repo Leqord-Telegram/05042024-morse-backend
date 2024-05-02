96ae837da3634994f19ae1484b96615e7f58a334
Leqord <illigarium@gmail.com>
2024-05-02 16:51:12 +1000

    Mut self

--- tg-backend-sls/src/storage/base.rs
+++ tg-backend-sls/src/storage/base.rs
@@ -7,26 +7,27 @@ use crate::model::category::*;
 
 pub trait Storage {
     async fn get_user(&self, id: Option<i64>, admin: Option<i64>) -> Result<Vec<User>, Error>;
-    async fn upsert_user(&self, user: User) -> Result<(), Error>;
-    async fn delete_user(&self, id: i64) -> Result<(), Error>;
+    async fn upsert_user(&mut self, user: User) -> Result<(), Error>;
+    async fn delete_user(&mut self, id: i64) -> Result<(), Error>;
 
     async fn get_product(&self, id: Option<i64>, category_id: Option<i64>, active: Option<bool>) -> Result<Vec<Product>, Error>;
-    async fn upsert_product(&self, product: Product) -> Result<(), Error>;
-    async fn delete_product(&self, id: i64) -> Result<(), Error>;
+    async fn upsert_product(&mut self, product: Product) -> Result<(), Error>;
+    async fn delete_product(&mut self, id: i64) -> Result<(), Error>;
 
     async fn get_order(&self, id: Option<i64>, user_id: Option<i64>, status: Option<Status>) -> Result<Vec<Order>, Status>;
-    async fn upsert_order(&self, order: Order) -> Result<(), Error>;
-    async fn delete_order(&self, id: i64) -> Result<(), Error>;
+    async fn upsert_order(&mut self, order: Order) -> Result<(), Error>;
+    async fn delete_order(&mut self, id: i64) -> Result<(), Error>;
 
     async fn get_image(&self, id: Option<i64>) -> Result<Vec<Image>, Error>;
-    async fn upsert_image(&self, image: Image) -> Result<(), Error>;
-    async fn delete_image(&self, id: i64) -> Result<(), Error>;
+    async fn upsert_image(&mut self, image: Image) -> Result<(), Error>;
+    async fn delete_image(&mut self, id: i64) -> Result<(), Error>;
 
     async fn get_category(&self, id: Option<i64>) -> Result<Vec<Category>, Error>;
-    async fn upsert_category(&self, category: Category) -> Result<(), Error>;
-    async fn delete_category(&self, id: i64) -> Result<(), Error>;
+    async fn upsert_category(&mut self, category: Category) -> Result<(), Error>;
+    async fn delete_category(&mut self, id: i64) -> Result<(), Error>;
 }
 
 pub enum Error {
     InternalError(String),
+    NotFoundError(String),
 }
--- tg-backend-sls/src/storage/memory.rs
+++ tg-backend-sls/src/storage/memory.rs
@@ -28,63 +28,78 @@ impl MemoryStorage {
 }
 
 impl Storage for MemoryStorage {
-    async fn get_user(&self, id: Option<i64>, admin: Option<i64>) -> Result<User, Error> {
+    async fn get_user(&self, id: Option<i64>, admin: Option<i64>) -> Result<Vec<User>, Error> {
+        let filtered_users = self.users.values()
+            .filter(|&user| {
+                id.map_or(true, |id_val| user.id == id_val) &&
+                admin.map_or(true, |admin_val| user.admin == admin_val)
+            })
+            .cloned()
+            .collect::<Vec<_>>();
+        Ok(filtered_users)
+    }
+
+    async fn upsert_user(&mut self, user: User) -> Result<(), Error> {
+        self.users.insert(user.id.to_string(), user);
+        Ok(())
+    }
+
+    async fn delete_user(&mut self, id: i64) -> Result<(), Error> {
+        self.users.remove(&id.to_string());
+        Ok(())
+    }
+
+    async fn get_product(&self, id: Option<i64>, 
+                        category_id: Option<i64>, 
+                        active: Option<bool>
+                    ) -> Result<Vec<Product>, Error> {
         todo!()
     }
 
-    async fn upsert_user(&self, user: User) -> Result<(), Error> {
+    async fn upsert_product(&mut self, product: Product) -> Result<(), Error> {
         todo!()
     }
 
-    async fn delete_user(&self, id: i64) -> Result<(), Error> {
+    async fn delete_product(&mut self, id: i64) -> Result<(), Error> {
         todo!()
     }
 
-    async fn get_product(&self, id: Option<i64>, category_id: Option<i64>, active: Option<bool>) -> Result<Product, Error> {
+    async fn get_order(&self, id: Option<i64>, 
+                            user_id: Option<i64>, 
+                            status: Option<Status>
+                        ) -> Result<Vec<Order>, Status> {
         todo!()
     }
 
-    async fn upsert_product(&self, product: Product) -> Result<(), Error> {
+    async fn upsert_order(&mut self, order: Order) -> Result<(), Error> {
         todo!()
     }
 
-    async fn delete_product(&self, id: i64) -> Result<(), Error> {
+    async fn delete_order(&mut self, id: i64) -> Result<(), Error> {
         todo!()
     }
 
-    async fn get_order(&self, id: Option<i64>, user_id: Option<i64>, status: Option<Status>) -> Result<Order, Status> {
+    async fn get_image(&self, id: Option<i64>) -> Result<Vec<Image>, Error> {
         todo!()
     }
 
-    async fn upsert_order(&self, order: Order) -> Result<(), Error> {
+    async fn upsert_image(&mut self, image: Image) -> Result<(), Error> {
         todo!()
     }
 
-    async fn delete_order(&self, id: i64) -> Result<(), Error> {
+    async fn delete_image(&mut self, id: i64) -> Result<(), Error> {
         todo!()
     }
 
-    async fn get_image(&self, id: Option<i64>) -> Result<Image, Error> {
+    async fn get_category(&self, id: Option<i64>) -> Result<Vec<Category>, Error> {
         todo!()
     }
 
-    async fn upsert_image(&self, image: Image) -> Result<(), Error> {
+    async fn upsert_category(&mut self, category: Category) -> Result<(), Error> {
         todo!()
     }
 
-    async fn delete_image(&self, id: i64) -> Result<(), Error> {
-        todo!()
-    }
-
-    async fn get_category(&self, id: Option<i64>) -> Result<Category, Error> {
-        todo!()
-    }
-
-    async fn upsert_category(&self, category: Category) -> Result<(), Error> {
-        todo!()
-    }
-
-    async fn delete_category(&self, id: i64) -> Result<(), Error> {
+    async fn delete_category(&mut self, id: i64) -> Result<(), Error> {
         todo!()
     }
 }
