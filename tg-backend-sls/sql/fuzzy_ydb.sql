270b6b85ea09436bb5856d2ccec310f1638f673c
Leqord <illigarium@gmail.com>
2024-05-02 23:48:14 +1000

    поиск

--- tg-backend-sls/sql/fuzzy_ydb.sql
+++ tg-backend-sls/sql/fuzzy_ydb.sql
@@ -1,23 +1,25 @@
--- select Unicode::LevensteinDistance("шоссеандра", "шоф") as distance
-$defsep = " ";
+$sep = " ";
+$filtr = "()-+=,.\'/\\|[]{}`~?!<>:*&^%$#@";
 
-$splitstr = ($str, $sep) -> {
-    return Unicode::SplitToList(Unicode::Fold($str), $sep)
+$splitstr = ($str) -> {
+    return Unicode::SplitToList(Unicode::Fold(Unicode::RemoveAll($str, $filtr), "Russian" AS Language), $sep)
 };
 
-$strRef = "шоссеандра   шла";
-$RefSplit = $splitstr($strRef, $defsep);
+$strRef = "шоссеандра   (шла)";
+$RefSplit = $splitstr($strRef);
 $RefTable = select source_word from (select $RefSplit  as source_word) flatten by source_word; 
 
 $strSearch = "шоссе шоа";
-$SearchSplit = $splitstr($strSearch, $defsep);
+$SearchSplit = $splitstr($strSearch);
 $SearchTable = select search_word from (select $SearchSplit  as search_word) flatten by search_word; 
 
 $compareTable = select * from $SearchTable as st cross join $RefTable as rt;
 $min_length = select min_of(Unicode::GetLength(search_word), Unicode::GetLength(source_word)) as min_length from $compareTable;
 
-select Unicode::Substring(source_word, 0, min_of(Unicode::GetLength(source_word), Unicode::GetLength(search_word))) as source_word_part, search_word from $compareTable;
+select * from $compareTable;
 
+-- мб и не надо или только для подсказок:
+-- select Unicode::Substring(source_word, 0, min_of(Unicode::GetLength(source_word), Unicode::GetLength(search_word))) as source_word_part, search_word from $compareTable;
 -- сформировать таблицу комбинаций запрос-источник
 -- хранить кеш по словам на стороне сервера в таблице с TTL
 -- для каждого слова запроса проверять наличие в кеше, если его нет, то искать уже полноценно
