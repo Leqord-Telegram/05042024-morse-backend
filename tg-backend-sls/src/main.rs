bf637af5d77b74521a0a38077a5c7873d0f2823e
Leqord <illigarium@gmail.com>
2024-05-03 14:10:02 +1000

    Пилим

--- tg-backend-sls/src/main.rs
+++ tg-backend-sls/src/main.rs
@@ -7,8 +7,12 @@ use storage::memory::*;
 use storage::base::*;
 
 use std::env;
+use std::sync::Arc;
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 use serde::Deserialize;
+use serde_json::json;
+use tokio::sync::Mutex;
+
 
 #[derive(Deserialize)]
 struct ProductQuery {
@@ -18,57 +22,58 @@ struct ProductQuery {
 }
 
 struct AppState {
-    storage: MemoryStorage,
+    storage: Arc<Mutex<MemoryStorage>>,
 }
 
 impl AppState {
     pub async fn new() -> Self {
-        let mut state = AppState {
-            storage: MemoryStorage::new(),
+        let state = AppState {
+            storage: Arc::new(Mutex::new(MemoryStorage::new())),
         };
-
-        let _ = state.storage.upsert_category(Category{
-            id: 123,
-            name: "Shit".to_string(),
-        }).await;
-
-        let _ = state.storage.upsert_category(Category{
-            id: 141,
-            name: "Show".to_string(),
-        }).await;
-
-        let _ = state.storage.upsert_product(Product{
-            id: 6123,
-            name: "Shit".to_string(),
-            destription: "Huita".to_string(),
-            category_id: 123,
-            price: 5123,
-            quantity: 22,
-            active: true,
-            images: vec![],
-        }).await;
 
         return state;
     }
 }
+
 
 #[get("/products")]
 async fn get_products(
     data: web::Data<AppState>,
     query: web::Query<ProductQuery>
 ) -> impl Responder {
+    let storage = data.storage.lock().await;
+
     let query_info = query.into_inner();
-    let products = data.storage.get_product(query_info.id, 
+    let products = storage.get_product(query_info.id, 
                                                             query_info.category_id, 
                                                             query_info.active
                                                         ).await.unwrap_or_else(|_| vec![]);
     HttpResponse::Ok().json(products)
 }
 
+#[post("/products")]
+async fn create_product(
+    data: web::Data<AppState>,
+    product: web::Json<Product>
+) -> HttpResponse {
+    let mut storage = data.storage.lock().await;
+
+    match storage.upsert_product(product.into_inner()).await {
+        Ok(_) => HttpResponse::Created().json(json!({"status": "success"})),
+        Err(e) =>
+            match e {
+                StorageError::InternalError(s) => HttpResponse::InternalServerError().json(json!({"status": "error", "details": s})),
+                StorageError::NotFoundError(s) => HttpResponse::InternalServerError().json(json!({"status": "not found", "details": s})),
+            }
+        
+    }
+}
 
 #[get("/categories")]
 async fn get_categories(data: web::Data<AppState>) -> impl Responder {
-    let categories = data.storage.get_category(None).await.unwrap_or_else(|_| vec![]);
+    let storage = data.storage.lock().await;
+
+    let categories = storage.get_category(None).await.unwrap_or_else(|_| vec![]);
     HttpResponse::Ok().json(categories)
 }
 
@@ -76,6 +81,7 @@ async fn get_categories(data: web::Data<AppState>) -> impl Responder {
 async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
 }
+
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
