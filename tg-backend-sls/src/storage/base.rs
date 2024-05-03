53b645186139aa93e9f538024ecf9ddc6e2f2e7e
Leqord <illigarium@gmail.com>
2024-05-03 13:46:13 +1000

    StorageError

--- tg-backend-sls/src/storage/base.rs
+++ tg-backend-sls/src/storage/base.rs
@@ -6,31 +6,31 @@ use crate::model::category::*;
 
 
 pub trait Storage {
-    async fn get_user(&self, id: Option<i64>, admin: Option<bool>) -> Result<Vec<User>, Error>;
-    async fn upsert_user(&mut self, user: User) -> Result<(), Error>;
-    async fn delete_user(&mut self, id: i64) -> Result<(), Error>;
+    async fn get_user(&self, id: Option<i64>, admin: Option<bool>) -> Result<Vec<User>, StorageError>;
+    async fn upsert_user(&mut self, user: User) -> Result<(), StorageError>;
+    async fn delete_user(&mut self, id: i64) -> Result<(), StorageError>;
 
-    async fn get_product(&self, id: Option<i64>, category_id: Option<i64>, active: Option<bool>) -> Result<Vec<Product>, Error>;
-    async fn upsert_product(&mut self, product: Product) -> Result<(), Error>;
-    async fn delete_product(&mut self, id: i64) -> Result<(), Error>;
+    async fn get_product(&self, id: Option<i64>, category_id: Option<i64>, active: Option<bool>) -> Result<Vec<Product>, StorageError>;
+    async fn upsert_product(&mut self, product: Product) -> Result<(), StorageError>;
+    async fn delete_product(&mut self, id: i64) -> Result<(), StorageError>;
 
-    async fn get_order(&self, id: Option<i64>, user_id: Option<i64>, status: Option<Status>) -> Result<Vec<Order>, Error>;
-    async fn upsert_order(&mut self, order: Order) -> Result<(), Error>;
-    async fn delete_order(&mut self, id: i64) -> Result<(), Error>;
+    async fn get_order(&self, id: Option<i64>, user_id: Option<i64>, status: Option<Status>) -> Result<Vec<Order>, StorageError>;
+    async fn upsert_order(&mut self, order: Order) -> Result<(), StorageError>;
+    async fn delete_order(&mut self, id: i64) -> Result<(), StorageError>;
 
-    async fn get_image(&self, id: Option<i64>) -> Result<Vec<Image>, Error>;
-    async fn upsert_image(&mut self, image: Image) -> Result<(), Error>;
-    async fn delete_image(&mut self, id: i64) -> Result<(), Error>;
+    async fn get_image(&self, id: Option<i64>) -> Result<Vec<Image>, StorageError>;
+    async fn upsert_image(&mut self, image: Image) -> Result<(), StorageError>;
+    async fn delete_image(&mut self, id: i64) -> Result<(), StorageError>;
 
-    async fn get_category(&self, id: Option<i64>) -> Result<Vec<Category>, Error>;
-    async fn upsert_category(&mut self, category: Category) -> Result<(), Error>;
-    async fn delete_category(&mut self, id: i64) -> Result<(), Error>;
+    async fn get_category(&self, id: Option<i64>) -> Result<Vec<Category>, StorageError>;
+    async fn upsert_category(&mut self, category: Category) -> Result<(), StorageError>;
+    async fn delete_category(&mut self, id: i64) -> Result<(), StorageError>;
 
-    async fn get_cart(&self, user_id: Option<i64>) -> Result<Vec<Cart>, Error>;
-    async fn upsert_cart(&mut self, cart: Cart) -> Result<(), Error>;
+    async fn get_cart(&self, user_id: Option<i64>) -> Result<Vec<Cart>, StorageError>;
+    async fn upsert_cart(&mut self, cart: Cart) -> Result<(), StorageError>;
 }
 
-pub enum Error {
+pub enum StorageError {
     InternalError(String),
     NotFoundError(String),
 }
--- tg-backend-sls/src/storage/memory.rs
+++ tg-backend-sls/src/storage/memory.rs
@@ -30,7 +30,7 @@ impl MemoryStorage {
 }
 
 impl Storage for MemoryStorage {
-    async fn get_user(&self, id: Option<i64>, admin: Option<bool>) -> Result<Vec<User>, Error> {
+    async fn get_user(&self, id: Option<i64>, admin: Option<bool>) -> Result<Vec<User>, StorageError> {
         let filtered_users = self.users.values()
             .filter(|&user| {
                 id.map_or(true, |id_val| user.id == id_val) &&
@@ -41,17 +41,17 @@ impl Storage for MemoryStorage {
         Ok(filtered_users)
     }
 
-    async fn upsert_user(&mut self, user: User) -> Result<(), Error> {
+    async fn upsert_user(&mut self, user: User) -> Result<(), StorageError> {
         self.users.insert(user.id.to_string(), user);
         Ok(())
     }
 
-    async fn delete_user(&mut self, id: i64) -> Result<(), Error> {
+    async fn delete_user(&mut self, id: i64) -> Result<(), StorageError> {
         self.users.remove(&id.to_string());
         Ok(())
     }
 
-    async fn get_product(&self, id: Option<i64>, category_id: Option<i64>, active: Option<bool>) -> Result<Vec<Product>, Error> {
+    async fn get_product(&self, id: Option<i64>, category_id: Option<i64>, active: Option<bool>) -> Result<Vec<Product>, StorageError> {
         let products = self.products.values()
             .filter(|product| 
                 id.map_or(true, |id_val| product.id == id_val) &&
@@ -63,19 +63,19 @@ impl Storage for MemoryStorage {
         Ok(products)
     }
 
-    async fn upsert_product(&mut self, product: Product) -> Result<(), Error> {
+    async fn upsert_product(&mut self, product: Product) -> Result<(), StorageError> {
         self.products.insert(product.id.to_string(), product);
         Ok(())
     }
 
-    async fn delete_product(&mut self, id: i64) -> Result<(), Error> {
+    async fn delete_product(&mut self, id: i64) -> Result<(), StorageError> {
         self.products.remove(&id.to_string()).map_or_else(
-            || Err(Error::NotFoundError(format!("Product with ID {} not found", id))),
+            || Err(StorageError::NotFoundError(format!("Product with ID {} not found", id))),
             |_| Ok(())
         )
     }
 
-    async fn get_cart(&self, user_id: Option<i64>) -> Result<Vec<Cart>, Error> {
+    async fn get_cart(&self, user_id: Option<i64>) -> Result<Vec<Cart>, StorageError> {
         let carts = self.carts.values()
         .filter(|cart| 
             user_id.map_or(true, |user_id| cart.user_id == user_id))
@@ -84,12 +84,12 @@ impl Storage for MemoryStorage {
     Ok(carts)
     }
 
-    async fn upsert_cart(&mut self, cart: Cart) -> Result<(), Error> {
+    async fn upsert_cart(&mut self, cart: Cart) -> Result<(), StorageError> {
         self.carts.insert(cart.user_id.to_string(), cart);
         Ok(())
     }
 
-    async fn get_order(&self, id: Option<i64>, user_id: Option<i64>, status: Option<Status>) -> Result<Vec<Order>, Error> {
+    async fn get_order(&self, id: Option<i64>, user_id: Option<i64>, status: Option<Status>) -> Result<Vec<Order>, StorageError> {
         let orders = self.orders.values()
             .filter(|order| 
                 id.map_or(true, |id_val| order.id == id_val) &&
@@ -101,19 +101,19 @@ impl Storage for MemoryStorage {
         Ok(orders)
     }
 
-    async fn upsert_order(&mut self, order: Order) -> Result<(), Error> {
+    async fn upsert_order(&mut self, order: Order) -> Result<(), StorageError> {
         self.orders.insert(order.id.to_string(), order);
         Ok(())
     }
 
-    async fn delete_order(&mut self, id: i64) -> Result<(), Error> {
+    async fn delete_order(&mut self, id: i64) -> Result<(), StorageError> {
         self.orders.remove(&id.to_string()).map_or_else(
-            || Err(Error::NotFoundError(format!("Order with ID {} not found", id))),
+            || Err(StorageError::NotFoundError(format!("Order with ID {} not found", id))),
             |_| Ok(())
         )
     }
 
-    async fn get_image(&self, id: Option<i64>) -> Result<Vec<Image>, Error> {
+    async fn get_image(&self, id: Option<i64>) -> Result<Vec<Image>, StorageError> {
         let images = self.images.values()
             .filter(|image| 
                 id.map_or(true, |id_val| image.id == id_val)
@@ -123,18 +123,18 @@ impl Storage for MemoryStorage {
         Ok(images)
     }
 
-    async fn upsert_image(&mut self, image: Image) -> Result<(), Error> {
+    async fn upsert_image(&mut self, image: Image) -> Result<(), StorageError> {
         self.images.insert(image.id.to_string(), image);
         Ok(())
     }
 
-    async fn delete_image(&mut self, id: i64) -> Result<(), Error> {
+    async fn delete_image(&mut self, id: i64) -> Result<(), StorageError> {
         self.images.remove(&id.to_string()).map_or_else(
-            || Err(Error::NotFoundError(format!("Image with ID {} not found", id))),
+            || Err(StorageError::NotFoundError(format!("Image with ID {} not found", id))),
             |_| Ok(())
         )
     }
-    async fn get_category(&self, id: Option<i64>) -> Result<Vec<Category>, Error> {
+    async fn get_category(&self, id: Option<i64>) -> Result<Vec<Category>, StorageError> {
         let categories = self.categories.values()
             .filter(|category| 
                 id.map_or(true, |id_val| category.id == id_val)
@@ -144,14 +144,14 @@ impl Storage for MemoryStorage {
         Ok(categories)
     }
 
-    async fn upsert_category(&mut self, category: Category) -> Result<(), Error> {
+    async fn upsert_category(&mut self, category: Category) -> Result<(), StorageError> {
         self.categories.insert(category.id.to_string(), category);
         Ok(())
     }
 
-    async fn delete_category(&mut self, id: i64) -> Result<(), Error> {
+    async fn delete_category(&mut self, id: i64) -> Result<(), StorageError> {
         self.categories.remove(&id.to_string()).map_or_else(
-            || Err(Error::NotFoundError(format!("Category with ID {} not found", id))),
+            || Err(StorageError::NotFoundError(format!("Category with ID {} not found", id))),
             |_| Ok(())
         )
     }
