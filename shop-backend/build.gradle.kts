0a93930f50627e9f784d3d8d5f74c825a0d44195
Leqord <illigarium@gmail.com>
2024-05-05 14:19:34 +1000

    Init

---
+++ shop-backend/build.gradle.kts
@@ -0,0 +1,49 @@
+val ktor_version: String by project
+val kotlin_version: String by project
+val logback_version: String by project
+
+plugins {
+    kotlin("jvm") version "1.9.23"
+    id("io.ktor.plugin") version "2.3.10"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.23"
+}
+
+group = "ru.morsianin_shop"
+version = "0.0.1"
+
+application {
+    mainClass.set("ru.morsianin_shop.ApplicationKt")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core-jvm")
+    implementation("io.ktor:ktor-server-auth-jvm")
+    implementation("io.ktor:ktor-server-auth-jwt-jvm")
+    implementation("io.ktor:ktor-server-sessions-jvm")
+    implementation("io.ktor:ktor-server-auto-head-response-jvm")
+    implementation("io.ktor:ktor-server-resources")
+    implementation("io.ktor:ktor-server-host-common-jvm")
+    implementation("io.ktor:ktor-server-status-pages-jvm")
+    implementation("io.ktor:ktor-server-compression-jvm")
+    implementation("io.ktor:ktor-server-conditional-headers-jvm")
+    implementation("io.ktor:ktor-server-default-headers-jvm")
+    implementation("com.ucasoft.ktor:ktor-simple-cache:0.+")
+    implementation("com.ucasoft.ktor:ktor-simple-redis-cache-jvm:0.+")
+    implementation("com.ucasoft.ktor:ktor-simple-memory-cache:0.+")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
+    implementation("org.postgresql:postgresql:42.5.1")
+    implementation("com.h2database:h2:2.1.214")
+    implementation("io.ktor:ktor-server-websockets-jvm")
+    implementation("io.ktor:ktor-server-netty-jvm")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    testImplementation("io.ktor:ktor-server-tests-jvm")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
