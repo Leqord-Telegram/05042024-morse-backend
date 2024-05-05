5da9dca29084d7768079080f54cf1f6a03b9ef1c
Leqord <illigarium@gmail.com>
2024-05-05 20:40:52 +1000

    Зависимость для статусов сервера

--- shop-backend/build.gradle.kts
+++ shop-backend/build.gradle.kts
@@ -30,4 +30,5 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
     implementation("io.ktor:ktor-serialization:1.5.4")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.1")
+    implementation("io.ktor:ktor-server-status-pages:$ktor_version")
 }
