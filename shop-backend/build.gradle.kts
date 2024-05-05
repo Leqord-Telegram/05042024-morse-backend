ca133a9fb96858a32be8480d12bace665f74d59c
Leqord <illigarium@gmail.com>
2024-05-05 20:59:30 +1000

    Обновлены зависимости, плагин для сериализации, зависимость для согласования контента

--- shop-backend/build.gradle.kts
+++ shop-backend/build.gradle.kts
@@ -5,6 +5,7 @@ val logback_version: String by project
 
 plugins {
     kotlin("jvm") version "1.9.23"
+    kotlin("plugin.serialization") version "1.9.10"
     id("io.ktor.plugin") version "2.3.10"
 }
 
@@ -26,9 +27,10 @@ dependencies {
     implementation("io.ktor:ktor-server-core-jvm")
     implementation("io.ktor:ktor-server-netty-jvm")
     implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("io.ktor:ktor-serialization:$ktor_version")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
+    implementation("io.ktor:ktor-server-content-negotiation:$ktor_version")
+    implementation("io.ktor:ktor-server-status-pages:$ktor_version")
     testImplementation("io.ktor:ktor-server-tests-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
-    implementation("io.ktor:ktor-serialization:1.5.4")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.1")
-    implementation("io.ktor:ktor-server-status-pages:$ktor_version")
 }
