f26b5d56686aa9df18f97a62fba8e2c34004e88f
Leqord <illigarium@gmail.com>
2024-05-05 20:39:33 +1000

    Базовый тест

---
+++ shop-backend/src/test/kotlin/ru/morsianin_shop/ApplicationTest.kt
@@ -0,0 +1,21 @@
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.server.testing.*
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import ru.morsianin_shop.module
+
+class ApplicationTest {
+
+    @Test
+    fun testRoot() = testApplication {
+        application {
+            module()
+        }
+        val response = client.get("/")
+
+        assertEquals(HttpStatusCode.OK, response.status)
+        assertEquals("Hello World!", response.bodyAsText())
+    }
+}
