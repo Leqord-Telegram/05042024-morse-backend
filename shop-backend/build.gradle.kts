faedd73fd0128bfd7b243b9e2dad011973f39fee
Leqord <illigarium@gmail.com>
2024-05-07 00:55:53 +1000

    Зависимость Postgres, рефактор зависимостей

--- shop-backend/build.gradle.kts
+++ shop-backend/build.gradle.kts
@@ -3,8 +3,9 @@ import java.util.*
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
-val tcnative_version = "2.0.65.Final"
 val exposed_version: String by project
+val tcnative_version = "2.0.65.Final"
+val postgres_version = "42.7.3"
 
 val osName = System.getProperty("os.name").lowercase(Locale.getDefault())
 val tcnative_classifier = when {
@@ -45,9 +46,10 @@ dependencies {
     implementation("io.ktor:ktor-server-resources:$ktor_version")
     implementation("io.ktor:ktor-server-auto-head-response:$ktor_version")
     implementation("io.ktor:ktor-server-rate-limit:$ktor_version")
-    implementation("org.jetbrains.exposed", "exposed-core", exposed_version)
-    implementation("org.jetbrains.exposed", "exposed-dao", exposed_version)
-    implementation("org.jetbrains.exposed", "exposed-jdbc", exposed_version)
+    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
+    implementation("org.postgresql:postgresql:$postgres_version")
 
     if (tcnative_classifier != null) {
         implementation("io.netty:netty-tcnative-boringssl-static:$tcnative_version:$tcnative_classifier")
