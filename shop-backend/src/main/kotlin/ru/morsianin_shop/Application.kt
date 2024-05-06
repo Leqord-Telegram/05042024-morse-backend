e121151a30693160336f8d34b794473da87a5269
Leqord <illigarium@gmail.com>
2024-05-07 01:42:55 +1000

    Оптимизированы импорты

--- shop-backend/src/main/kotlin/ru/morsianin_shop/Application.kt
+++ shop-backend/src/main/kotlin/ru/morsianin_shop/Application.kt
@@ -3,14 +3,11 @@ package ru.morsianin_shop
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import io.ktor.server.plugins.ratelimit.*
-import org.jetbrains.exposed.sql.transactions.transaction
 import ru.morsianin_shop.plugins.configureOther
 import ru.morsianin_shop.plugins.configureRateLimit
 import ru.morsianin_shop.plugins.configureResources
 import ru.morsianin_shop.plugins.configureRouting
 import ru.morsianin_shop.routes.*
-import ru.morsianin_shop.storage.DatabaseSingleton
 import ru.morsianin_shop.storage.configureStorage
 
 fun main() {
