e14c4f33a4715a5ea2bac935e7e3d1aecea3164f
Leqord <illigarium@gmail.com>
2024-05-07 01:32:40 +1000

    MCPool удалён

--- shop-backend/src/main/kotlin/ru/morsianin_shop/storage/DatabaseSingleton.kt
+++ shop-backend/src/main/kotlin/ru/morsianin_shop/storage/DatabaseSingleton.kt
@@ -1,7 +1,5 @@
 package ru.morsianin_shop.storage
 
-import com.zaxxer.hikari.HikariConfig
-import com.zaxxer.hikari.HikariDataSource
 import io.ktor.server.application.*
 import kotlinx.coroutines.Dispatchers
 import org.jetbrains.exposed.sql.Database
@@ -11,24 +9,8 @@ import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransacti
 import org.jetbrains.exposed.sql.transactions.transaction
 
 object DatabaseSingleton {
-    fun init(driver: String, url: String, user: String, pass: String, maxPool: Int = 6) {
-        val config = HikariConfig().apply {
-            jdbcUrl = url
-            driverClassName = driver
-            username = user
-            password = pass
-            maximumPoolSize = maxPool
-            isReadOnly = false
-            transactionIsolation = "TRANSACTION_SERIALIZABLE"
-        }
-
-        val dataSource = HikariDataSource(config)
-
-        Database.connect(
-            datasource = dataSource,
-            databaseConfig = DatabaseConfig {
-            }
-        )
+    fun init(driver: String, url: String, user: String, pass: String) {
+        Database.connect(url, driver, user, pass)
     }
 
     suspend fun <T> dbQuery(block: suspend () -> T): T =
@@ -36,11 +18,19 @@ object DatabaseSingleton {
 }
 
 fun Application.configureStorage() {
+    val driver = "org.postgresql.Driver"
+    val url = "jdbc:postgresql://localhost:5432/shop"
+    val user = System.getenv("DB_USER") ?: "postgres"
+    val password = System.getenv("DB_PASSWORD") ?: "postgres"
+
+    log.info("Connecting to: $url user: $user driver: $driver")
+
     DatabaseSingleton.init(
-        "org.postgresql.Driver",
-        "jdbc:postgresql://localhost:5432/shop",
-        System.getenv("DB_USER") ?: "postgres",
-        System.getenv("DB_PASSWORD") ?: "postgres");
+        driver=driver,
+        url=url,
+        user=user,
+        pass=password
+    )
 
     log.info("Storage initialized")
 }
