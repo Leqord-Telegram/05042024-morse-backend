03c3b452c030aa1087eebe095b88ec0f0520c032
Leqord <illigarium@gmail.com>
2024-05-06 20:05:08 +1000

    Рефактор модели Order

--- shop-backend/src/main/kotlin/ru/morsianin_shop/resources/Order.kt
+++ shop-backend/src/main/kotlin/ru/morsianin_shop/resources/Order.kt
@@ -1,14 +1,13 @@
 package ru.morsianin_shop.resources
 
 import io.ktor.resources.*
-import ru.morsianin_shop.model.OrderItem
-import ru.morsianin_shop.model.OrderStatus
+import ru.morsianin_shop.model.Order
 
 @Resource("/orders")
 class Orders(
     val userId: Long? = null,
-    val items: List<OrderItem>? = null,
-    val status: OrderStatus? = null
+    val items: List<Order.OrderItem>? = null,
+    val status: Order.OrderStatus? = null
 )   {
     @Resource("{id}")
     class Id(val parent: Orders = Orders(), val id: Long)
