6f1361e5c33a1150d9041dc75e36144a2f195599
Leqord <illigarium@gmail.com>
2024-05-06 20:04:15 +1000

    Рефактор модели Order

--- shop-backend/src/main/kotlin/ru/morsianin_shop/model/Order.kt
+++ shop-backend/src/main/kotlin/ru/morsianin_shop/model/Order.kt
@@ -3,20 +3,20 @@ package ru.morsianin_shop.model
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class OrderItem(
-    val productId: Long,
-    val quantity: Long
-)
-
-@Serializable
-enum class OrderStatus {
-    Failed, Pending, Shipping, Arrived, Finished
-}
-
-@Serializable
 data class Order(
     val id: Long,
     val userId: Long,
     val items: List<OrderItem>,
     val status: OrderStatus
-)
+) {
+    @Serializable
+    data class OrderItem(
+        val productId: Long,
+        val quantity: Long
+    )
+
+    @Serializable
+    enum class OrderStatus {
+        Failed, Pending, Shipping, Arrived, Finished
+    }
+}
