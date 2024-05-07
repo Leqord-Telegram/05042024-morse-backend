81e1a99ea8437c9532afc8377054fb319591cbeb
Leqord <illigarium@gmail.com>
2024-05-08 00:34:28 +1000

    Создан StoredProduct.kt

---
+++ shop-backend/src/main/kotlin/ru/morsianin_shop/storage/StoredProduct.kt
@@ -0,0 +1,27 @@
+package ru.morsianin_shop.storage
+
+import org.jetbrains.exposed.dao.IntEntity
+import org.jetbrains.exposed.dao.IntEntityClass
+import org.jetbrains.exposed.dao.id.EntityID
+import org.jetbrains.exposed.dao.id.IntIdTable
+import org.jetbrains.exposed.sql.Table.Dual.references
+import ru.morsianin_shop.storage.StoredCategory.Companion.referrersOn
+
+object StoredProducts: IntIdTable() {
+    val name = text("name")
+    val description = text("description")
+    val category = reference("category", StoredCategories)
+    val price = integer("price")
+    val quantity = integer("quantity")
+    val active = bool("active")
+}
+
+class StoredProduct(id: EntityID<Int>) : IntEntity(id) {
+    companion object : IntEntityClass<StoredProduct>(StoredProducts)
+    var name by StoredProducts.name
+    var description by StoredProducts.description
+    var category by StoredCategory referencedOn StoredProducts.category
+    var price by StoredProducts.price
+    var quantity by StoredProducts.quantity
+    var active by StoredProducts.active
+}
