15361c4891987b6e269d1e592c62205fa6dd68af
Leqord <illigarium@gmail.com>
2024-05-08 01:07:44 +1000

    Добавлены специфичные для Telegram идентификаторы

--- shop-backend/src/main/kotlin/ru/morsianin_shop/storage/StoredUser.kt
+++ shop-backend/src/main/kotlin/ru/morsianin_shop/storage/StoredUser.kt
@@ -10,11 +10,13 @@ import org.jetbrains.exposed.dao.id.LongIdTable
 
 object StoredUsers: LongIdTable() {
     val name = text("name")
-    val username = text("username")
+    val tgId = long("tg_id").nullable().uniqueIndex()
+    val tgUsername = text("tg_username").nullable()
 }
 
 class StoredUser(id: EntityID<Long>) : LongEntity(id) {
     companion object : LongEntityClass<StoredUser>(StoredUsers)
     var name by StoredUsers.name
-    var username by StoredUsers.username
+    val tgId by StoredUsers.tgId
+    var tgUsername by StoredUsers.tgUsername
 }
