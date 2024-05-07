c112c64dbe750da34e0f3a246b7b9cd42ea006db
Leqord <illigarium@gmail.com>
2024-05-08 02:03:47 +1000

    Фикс: отсутствовала ссылка на Order

--- shop-backend/src/main/kotlin/ru/morsianin_shop/storage/StoredOrderItem.kt
+++ shop-backend/src/main/kotlin/ru/morsianin_shop/storage/StoredOrderItem.kt
@@ -6,12 +6,14 @@ import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.LongIdTable
 
 object StoredOrderItems: LongIdTable() {
+    val order = reference("order_id", StoredOrders)
     val product = reference("product_id", StoredProducts)
     val quantity = integer("quantity")
 }
 
 class StoredOrderItem(id: EntityID<Long>) : LongEntity(id) {
     companion object : LongEntityClass<StoredOrderItem>(StoredOrderItems)
+    var order by StoredOrder referencedOn StoredOrderItems.order
     var product by StoredProduct referencedOn StoredOrderItems.product
     var quantity by StoredOrderItems.quantity
 }
