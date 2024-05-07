efc1ee6ff44b451c18ba7ce2e5eeae4687dc3035
Leqord <illigarium@gmail.com>
2024-05-08 00:52:41 +1000

    Создан StoredOrder.kt

---
+++ shop-backend/src/main/kotlin/ru/morsianin_shop/storage/StoredOrder.kt
@@ -0,0 +1,19 @@
+package ru.morsianin_shop.storage
+
+import org.jetbrains.exposed.dao.IntEntity
+import org.jetbrains.exposed.dao.IntEntityClass
+import org.jetbrains.exposed.dao.id.EntityID
+import org.jetbrains.exposed.dao.id.IntIdTable
+import ru.morsianin_shop.model.Order
+import ru.morsianin_shop.resources.Users
+
+object StoredOrders: IntIdTable() {
+    val user = reference("user", StoredUsers)
+    val status = enumerationByName<Order.Status>("status", 255)
+}
+
+class StoredOrder(id: EntityID<Int>) : IntEntity(id) {
+    companion object : IntEntityClass<StoredOrder>(StoredOrders)
+    var user by StoredUser referencedOn StoredOrders.user
+    var status by StoredOrders.status
+}
