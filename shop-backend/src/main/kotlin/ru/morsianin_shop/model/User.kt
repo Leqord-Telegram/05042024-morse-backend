a4bed272d87f392bca64246df820195524b0deae
Leqord <illigarium@gmail.com>
2024-05-08 01:18:31 +1000

    Добавлена настройка привилегий

--- shop-backend/src/main/kotlin/ru/morsianin_shop/model/User.kt
+++ shop-backend/src/main/kotlin/ru/morsianin_shop/model/User.kt
@@ -7,12 +7,16 @@ data class User(
     val id: Long,
     val name: String,
     val username: String,
-    val admin: Boolean,
+    val privileges: Set<Privilege>,
     val cart: Cart
 ) {
     @Serializable
     data class Cart(val items: List<Item> = emptyList()) {
         @Serializable
         data class Item(val productId: Long, val quantity: Long)
+    }
+    @Serializable
+    enum class Privilege(val value: String) {
+        ADMIN("admin"),
     }
 }
