4ee6084e7800999e15759bf4cf7c99565b511a6b
Leqord <illigarium@gmail.com>
2024-05-08 01:13:41 +1000

    Создан StoredUserCartItem.kt

---
+++ shop-backend/src/main/kotlin/ru/morsianin_shop/storage/StoredUserCartItem.kt
@@ -0,0 +1,19 @@
+package ru.morsianin_shop.storage
+
+import org.jetbrains.exposed.dao.LongEntity
+import org.jetbrains.exposed.dao.LongEntityClass
+import org.jetbrains.exposed.dao.id.EntityID
+import org.jetbrains.exposed.dao.id.LongIdTable
+
+object StoredUserCartItems: LongIdTable() {
+    val user = reference("user_id", StoredUsers)
+    val product = reference("product_id", StoredProducts)
+    val quantity = integer("quantity")
+}
+
+class StoredUserCartItem(id: EntityID<Long>) : LongEntity(id) {
+    companion object : LongEntityClass<StoredUserCartItem>(StoredUserCartItems)
+    var user by StoredUser referencedOn StoredUserCartItems.user
+    var product by StoredProduct referencedOn StoredUserCartItems.product
+    var quantity by StoredUserCartItems.quantity
+}
