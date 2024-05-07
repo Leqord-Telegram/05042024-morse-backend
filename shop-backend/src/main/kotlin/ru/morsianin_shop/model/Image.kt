be1fa49211b17cb9e25d767a39ce5f4bf7f793bb
Leqord <illigarium@gmail.com>
2024-05-07 23:10:46 +1000

    Переименовано поле Image

--- shop-backend/src/main/kotlin/ru/morsianin_shop/model/Image.kt
+++ shop-backend/src/main/kotlin/ru/morsianin_shop/model/Image.kt
@@ -1,9 +1,10 @@
 package ru.morsianin_shop.model
 
+import io.ktor.http.*
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class Image(
     val id: Long,
-    val link: String
+    val url: String
 )
