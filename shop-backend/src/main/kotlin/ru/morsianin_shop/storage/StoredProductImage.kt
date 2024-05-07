831dc5ad3b8486e08622c0845d741141da244c20
Leqord <illigarium@gmail.com>
2024-05-08 01:33:31 +1000

    Оптимизация импортов

--- shop-backend/src/main/kotlin/ru/morsianin_shop/storage/StoredProductImage.kt
+++ shop-backend/src/main/kotlin/ru/morsianin_shop/storage/StoredProductImage.kt
@@ -4,9 +4,6 @@ import org.jetbrains.exposed.dao.LongEntity
 import org.jetbrains.exposed.dao.LongEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.LongIdTable
-import org.jetbrains.exposed.sql.ReferenceOption
-import ru.morsianin_shop.resources.Images
-import ru.morsianin_shop.resources.Products
 
 object StoredProductImages: LongIdTable() {
     val product = reference("product_id", StoredProducts)
