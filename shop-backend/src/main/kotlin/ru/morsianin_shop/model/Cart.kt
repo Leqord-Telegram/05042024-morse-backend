f53b8e0e19a66d075340d96d630749a563a3f6dc
Leqord <illigarium@gmail.com>
2024-05-08 01:04:04 +1000

    Создан Cart.kt

---
+++ shop-backend/src/main/kotlin/ru/morsianin_shop/model/Cart.kt
@@ -0,0 +1,15 @@
+package ru.morsianin_shop.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Cart(
+    val userId: Long,
+    val items: List<Item>,
+) {
+    @Serializable
+    data class Item(
+        val productId: Long,
+        val quantity: Long,
+    )
+}
