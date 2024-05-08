62461f0f9ee7c3961552fd53c365de973e878ac8
Leqord <illigarium@gmail.com>
2024-05-09 03:12:03 +1000

    Рефактор

--- shop-backend/src/main/kotlin/ru/morsianin_shop/storage/StoredUserPrivilege.kt
+++ shop-backend/src/main/kotlin/ru/morsianin_shop/storage/StoredUserPrivilege.kt
@@ -5,10 +5,11 @@ import org.jetbrains.exposed.dao.LongEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.LongIdTable
 import ru.morsianin_shop.model.UserDTO
+import ru.morsianin_shop.model.UserPrivilegeDTO
 
 object StoredUserPrivileges: LongIdTable("user_privilege") {
     val user = reference("user_id", StoredUsers)
-    val privilege = enumerationByName<UserDTO.Privilege>("privilege", 255)
+    val privilege = enumerationByName<UserPrivilegeDTO>("privilege", 255)
 }
 
 class StoredUserPrivilege(id: EntityID<Long>) : LongEntity(id) {
