238d65a2e54d2d00edb02b3a49556f04e2139184
Leqord <illigarium@gmail.com>
2024-05-09 03:28:38 +1000

    Фикс

--- shop-backend/src/main/kotlin/ru/morsianin_shop/resources/User.kt
+++ shop-backend/src/main/kotlin/ru/morsianin_shop/resources/User.kt
@@ -2,12 +2,13 @@ package ru.morsianin_shop.resources
 
 import io.ktor.resources.*
 import ru.morsianin_shop.model.OrderDTO
+import ru.morsianin_shop.model.OrderItemDTO
 
 @Resource("/users")
 class Users(
     val name: String? = null,
     val admin: Boolean? = null,
-    val cart: List<OrderDTO.Item>? = null,
+    val cart: List<OrderItemDTO>? = null,
 )   {
     @Resource("{id}")
     class Id(val parent: Users = Users(), val id: Long) {
