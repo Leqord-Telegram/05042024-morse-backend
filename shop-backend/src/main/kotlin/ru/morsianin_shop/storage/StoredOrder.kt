d9b2dbc1aa6044475b66be0525417e936ea40235
Leqord <illigarium@gmail.com>
2024-05-09 02:02:42 +1000

    Поле для order-to-items

--- shop-backend/src/main/kotlin/ru/morsianin_shop/storage/StoredOrder.kt
+++ shop-backend/src/main/kotlin/ru/morsianin_shop/storage/StoredOrder.kt
@@ -5,6 +5,7 @@ import org.jetbrains.exposed.dao.LongEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.LongIdTable
 import ru.morsianin_shop.model.OrderDTO
+import ru.morsianin_shop.storage.StoredOrder.Companion.referrersOn
 
 object StoredOrders: LongIdTable("order") {
     val user = reference("user_id", StoredUsers)
@@ -15,4 +16,5 @@ class StoredOrder(id: EntityID<Long>) : LongEntity(id) {
     companion object : LongEntityClass<StoredOrder>(StoredOrders)
     var user by StoredUser referencedOn StoredOrders.user
     var status by StoredOrders.status
+    val items by StoredOrderItem referrersOn StoredOrderItems.order
 }
