b034addf9c5f4607f68ce3e7d623e511713f27c9
Leqord <illigarium@gmail.com>
2024-05-09 03:28:05 +1000

    Фикс

--- shop-backend/src/main/kotlin/ru/morsianin_shop/resources/Order.kt
+++ shop-backend/src/main/kotlin/ru/morsianin_shop/resources/Order.kt
@@ -2,12 +2,14 @@ package ru.morsianin_shop.resources
 
 import io.ktor.resources.*
 import ru.morsianin_shop.model.OrderDTO
+import ru.morsianin_shop.model.OrderItemDTO
+import ru.morsianin_shop.model.OrderStatusDTO
 
 @Resource("/orders")
 class Orders(
     val userId: Long? = null,
-    val items: List<OrderDTO.Item>? = null,
-    val status: OrderDTO.Status? = null
+    val items: List<OrderItemDTO>? = null,
+    val status: OrderStatusDTO? = null
 )   {
     @Resource("{id}")
     class Id(val parent: Orders = Orders(), val id: Long)
