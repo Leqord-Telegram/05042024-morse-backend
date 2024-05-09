4a5feab7784905e3934dd703d3b581be6ad8ef3d
Leqord <illigarium@gmail.com>
2024-05-10 02:16:30 +1000

    Реализация маршрута для пользователя

--- shop-backend/src/main/kotlin/ru/morsianin_shop/resources/User.kt
+++ shop-backend/src/main/kotlin/ru/morsianin_shop/resources/User.kt
@@ -3,10 +3,14 @@ package ru.morsianin_shop.resources
 import io.ktor.resources.*
 import ru.morsianin_shop.model.OrderItemResponse
 import ru.morsianin_shop.model.OrderStatus
+import ru.morsianin_shop.model.UserPrivilege
 
 @Resource("/users")
 class UserRequest(
     val name: String? = null,
-    val admin: Boolean? = null,
-    val cart: List<OrderItemResponse>? = null,
-)
+    //val admin: Boolean? = null,
+    //val privileges: UserPrivilege?,
+) {
+    @Resource("{id}")
+    class Id(val parent: UserRequest = UserRequest(), val id: Long)
+}
--- shop-backend/src/main/kotlin/ru/morsianin_shop/routes/User.kt
+++ shop-backend/src/main/kotlin/ru/morsianin_shop/routes/User.kt
@@ -5,15 +5,49 @@ import io.ktor.server.application.*
 import io.ktor.server.resources.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.routing
+import org.jetbrains.exposed.sql.Op
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import org.jetbrains.exposed.sql.and
+import ru.morsianin_shop.mapping.Mapper.mapToDTO
 import ru.morsianin_shop.resources.UserRequest
+import ru.morsianin_shop.storage.DatabaseStorage.dbQuery
+import ru.morsianin_shop.storage.StoredCategories
+import ru.morsianin_shop.storage.StoredUser
+import ru.morsianin_shop.storage.StoredUsers
 
 fun Application.userRoutes() {
     routing {
-        get<UserRequest> { users ->
-            call.respondText("tbi", status = HttpStatusCode.NotImplemented)
+        get<UserRequest> { filter ->
+            var query: Op<Boolean> = Op.TRUE
+
+            filter.name?.let {
+                query = query and (StoredUsers.name eq it)
+            }
+
+            dbQuery {
+                val found = StoredUser.find {
+                    query
+                }
+
+                if (!found.empty()) {
+                    call.respond(found.map { mapToDTO(it) }.toList())
+                }
+                else {
+                    call.respond(HttpStatusCode.NoContent)
+                }
+            }
         }
         get<UserRequest.Id> { id ->
-            call.respondText("tbi", status = HttpStatusCode.NotImplemented)
+            dbQuery {
+                val found = StoredUser.findById(id.id)
+
+                if (found != null) {
+                    call.respond(mapToDTO(found))
+                }
+                else {
+                    call.respond(HttpStatusCode.NotFound)
+                }
+            }
         }
     }
 }
