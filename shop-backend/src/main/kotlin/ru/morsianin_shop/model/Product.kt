b0f9da97ee32e17ee5650c6ccf872789c24195e7
Leqord <illigarium@gmail.com>
2024-05-10 01:31:50 +1000

    Рефактор

--- shop-backend/src/main/kotlin/ru/morsianin_shop/model/Product.kt
+++ shop-backend/src/main/kotlin/ru/morsianin_shop/model/Product.kt
@@ -11,5 +11,16 @@ data class ProductResponse(
     val price: Long,
     val quantity: Long,
     val active: Boolean,
-    val imageResponses: List<Image>
+    val imageResponses: List<ImageResponse>
+)
+
+@Serializable
+data class ProductRequest(
+    val name: String,
+    val description: String,
+    val categoryId: Long,
+    val price: Long,
+    val quantity: Long,
+    val active: Boolean,
+    val imageIds: List<Long>
 )
