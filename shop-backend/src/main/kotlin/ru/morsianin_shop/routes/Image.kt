4413399e5678db87b1bf2c67dfa1144b6427e7a7
Leqord <illigarium@gmail.com>
2024-05-10 01:03:19 +1000

    Реализация маршрутов для image

--- shop-backend/src/main/kotlin/ru/morsianin_shop/routes/Image.kt
+++ shop-backend/src/main/kotlin/ru/morsianin_shop/routes/Image.kt
@@ -5,24 +5,50 @@ import io.ktor.server.application.*
 import io.ktor.server.resources.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.routing
+import ru.morsianin_shop.mapping.Mapper.mapToDTO
 import ru.morsianin_shop.resources.ImageRequest
+import ru.morsianin_shop.storage.DatabaseStorage.dbQuery
+import ru.morsianin_shop.storage.StoredImage
 
 fun Application.imageRoutes() {
     routing {
         get<ImageRequest> { images->
-            call.respondText("tbi", status = HttpStatusCode.NotImplemented)
+            dbQuery {
+                call.respond(StoredImage.all().map { img -> mapToDTO(img) })
+            }
         }
         post<ImageRequest> { images->
+            // TODO: implement
             call.respondText("tbi", status = HttpStatusCode.NotImplemented)
         }
         get<ImageRequest.Id> { id ->
-            call.respondText("tbi", status = HttpStatusCode.NotImplemented)
+            dbQuery {
+                val candidate = StoredImage.all().singleOrNull()
+
+                if (candidate != null) {
+                    call.respond(mapToDTO(candidate))
+                }
+                else {
+                    call.respond(HttpStatusCode.NotFound)
+                }
+            }
         }
         put<ImageRequest.Id> { id ->
+            // TODO: implement
             call.respondText("tbi", status = HttpStatusCode.NotImplemented)
         }
         delete<ImageRequest.Id> { id ->
-            call.respondText("tbi", status = HttpStatusCode.NotImplemented)
+            dbQuery {
+                val candidate = StoredImage.findById(id.id)
+
+                if (candidate != null) {
+                    candidate.delete()
+                    call.respond(HttpStatusCode.OK)
+                }
+                else {
+                    call.respond(HttpStatusCode.NotFound)
+                }
+            }
         }
     }
 }
