2c4826dcaa79d91fd81adf5bd1cf39e8fe23c3a5
Leqord <illigarium@gmail.com>
2024-05-09 18:31:22 +1000

    Рефактор

--- shop-backend/src/main/kotlin/ru/morsianin_shop/routes/Category.kt
+++ shop-backend/src/main/kotlin/ru/morsianin_shop/routes/Category.kt
@@ -5,20 +5,30 @@ import io.ktor.server.application.*
 import io.ktor.server.resources.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.routing
+import org.jetbrains.exposed.sql.Op
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import org.jetbrains.exposed.sql.and
+import org.jetbrains.exposed.sql.or
 import ru.morsianin_shop.mapping.Mapper.mapToDTO
 import ru.morsianin_shop.model.CategoriesDTO
 import ru.morsianin_shop.resources.Categories
 import ru.morsianin_shop.storage.DatabaseStorage.dbQuery
+import ru.morsianin_shop.storage.StoredCategories
 import ru.morsianin_shop.storage.StoredCategory
+import javax.management.Query.eq
 
 fun Application.categoryRoutes() {
     routing {
-        get<Categories> { categories ->
+        get<Categories> { filter ->
             val response = dbQuery {
-                StoredCategory.find {
-                    Stored
+                    var query: Op<Boolean> = Op.TRUE
+
+                    filter.name?.let {
+                        query = query and (StoredCategories.name eq it)
+                    }
+
+                    StoredCategory.find { query }
                 }
-            }
             call.respond(response)
         }
         post<Categories> { category ->
