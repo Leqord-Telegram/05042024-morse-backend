9c19c72c5d365e76727825ea6473214faaf702e2
Leqord <illigarium@gmail.com>
2024-05-09 20:09:00 +1000

    Эндпоинты категорий

--- shop-backend/src/main/kotlin/ru/morsianin_shop/routes/Category.kt
+++ shop-backend/src/main/kotlin/ru/morsianin_shop/routes/Category.kt
@@ -40,13 +40,50 @@ fun Application.categoryRoutes() {
             call.respond(HttpStatusCode.Created)
         }
         get<CategoryRequest.Id> { id ->
-            call.respondText("tbi", status = HttpStatusCode.NotImplemented)
+            var query: Op<Boolean> = Op.TRUE
+
+            query = query and (StoredCategories.id eq id.id)
+
+            id.parent.name?.let {
+                query = query and (StoredCategories.name eq it)
+            }
+
+            val response = dbQuery {
+                StoredCategory.find { query }.map { category -> mapToDTO(category) }.singleOrNull()
+            }
+
+            call.respond(response?: HttpStatusCode.NotFound)
         }
         put<CategoryRequest.Id> { id ->
-            call.respondText("tbi", status = HttpStatusCode.NotImplemented)
+            val newCategory = call.receive<CategoryNew>()
+
+            dbQuery {
+                val candidate = StoredCategory.findById(id.id)
+
+                if (candidate != null) {
+                    candidate.name = newCategory.name
+                    call.respond(HttpStatusCode.OK)
+                }
+                else {
+                    StoredCategory.new(id.id) {
+                        name = newCategory.name
+                    }
+                    call.respond(HttpStatusCode.Created)
+                }
+            }
         }
         delete<CategoryRequest.Id> { id ->
-            call.respondText("tbi", status = HttpStatusCode.NotImplemented)
+            dbQuery {
+                val candidate = StoredCategory.findById(id.id)
+                if (candidate != null) {
+                    candidate.delete()
+                    call.respond(HttpStatusCode.OK)
+                }
+                else {
+                    call.respond(HttpStatusCode.NotFound)
+                }
+
+            }
         }
     }
 }
