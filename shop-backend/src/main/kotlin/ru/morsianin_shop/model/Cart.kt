b973d489e5ba3d16e5e3c13db7527eab5ecb7d78
Leqord <illigarium@gmail.com>
2024-05-10 18:45:36 +1000

    Сущность самой корзины убрана

--- shop-backend/src/main/kotlin/ru/morsianin_shop/model/Cart.kt
+++ shop-backend/src/main/kotlin/ru/morsianin_shop/model/Cart.kt
@@ -2,11 +2,6 @@ package ru.morsianin_shop.model
 
 import kotlinx.serialization.Serializable
 
-@Serializable
-data class Cart(
-    val userId: Long,
-    val items: List<CartItemResponse>
-)
 
 @Serializable
 data class CartItemResponse(
