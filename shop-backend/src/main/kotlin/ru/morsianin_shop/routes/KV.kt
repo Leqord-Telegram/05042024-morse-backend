8a9bb4a950b57a54e33740a3d51a0b3fc74d7775
Leqord <illigarium@gmail.com>
2024-06-27 02:46:29 +1000

    Фиксы

--- shop-backend/src/main/kotlin/ru/morsianin_shop/routes/KV.kt
+++ shop-backend/src/main/kotlin/ru/morsianin_shop/routes/KV.kt
@@ -1,14 +1,6 @@
 package ru.morsianin_shop.routes
 
-import org.jetbrains.exposed.dao.LongEntity
-import org.jetbrains.exposed.dao.id.EntityID
-import ru.morsianin_shop.CANCEL_DURATION_KV_ID
-import ru.morsianin_shop.resources.KVRequest
-
-package ru.morsianin_shop.routes
-
 import io.ktor.http.*
-import io.ktor.http.content.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.auth.jwt.*
@@ -16,14 +8,12 @@ import io.ktor.server.request.*
 import io.ktor.server.resources.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.routing
-import ru.morsianin_shop.mapping.Mapper.mapToResponse
-import ru.morsianin_shop.model.ImageFormat
-import ru.morsianin_shop.model.ResultCreated
+import ru.morsianin_shop.CANCEL_DURATION_KV_ID
 import ru.morsianin_shop.model.UserPrivilege
 import ru.morsianin_shop.plugins.hasPrivilege
-import ru.morsianin_shop.resources.ImageRequest
-import ru.morsianin_shop.storage.*
+import ru.morsianin_shop.resources.KVRequest
 import ru.morsianin_shop.storage.DatabaseStorage.dbQuery
+import ru.morsianin_shop.storage.StoredKV
 
 
 fun Application.KVRoutes() {
