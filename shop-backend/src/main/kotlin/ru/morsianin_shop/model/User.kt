4dd3bb59a9a453afea5d2d6db9be9503bb7ff0b5
Leqord <illigarium@gmail.com>
2024-06-30 15:39:31 +1000

    Фиксы

--- shop-backend/src/main/kotlin/ru/morsianin_shop/model/User.kt
+++ shop-backend/src/main/kotlin/ru/morsianin_shop/model/User.kt
@@ -5,7 +5,7 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class UserResponse(
     val id: Long,
-    val name: String,
+    val name: String?,
     val privileges: Set<UserPrivilege>,
 )
 
