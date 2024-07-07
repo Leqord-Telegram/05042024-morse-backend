aee56674d19d0ff975e19faa3cc36992c1c28168
illigarium <illigarium@gmail.com>
2024-07-07 19:35:19 +1000

    sd

--- shop-backend/src/main/kotlin/ru/morsianin_shop/Application.kt
+++ shop-backend/src/main/kotlin/ru/morsianin_shop/Application.kt
@@ -36,7 +36,7 @@ import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter.ISO_LOCAL_DATE_TIME
 
 val CANCEL_DURATION_KV_ID: Long = 4123;
-val ORDER_CHAT_ID: Long = -4277372202;
+val ORDER_CHAT_ID: Long = -1002172780422;
 val ABOUT_US_KV_ID: Long = 5163;
 
 val bot = TelegramBot(System.getenv("TG_BOT_TOKEN"))
